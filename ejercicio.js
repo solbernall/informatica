0eb7efb585f98e2fe263bf8fcba9fed24bc8b56b
solbernall <ms.bernalcruz@colegiocalasanz.edu.ar>
2025-09-18 15:07:03 -0300

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,9 @@
+console.log("ejercicio1.html");
+let boton=document
+getElementById("boton");
+let parrafo=document
+getElementById("texto");
+btn.addEventListener("click",
+function(){
+    parrafo.innerHTML="Marynellis";
+});
